aa6583cb50cb9ca7609321dc62fb5de3d6ab8b91
Richardson Christian <rizinhof5@gmail.com>
2021-09-23 13:52:04 -0300

    comandos funcionais,so da uma melhorada na fala

--- JARVIS/main.py
+++ JARVIS/main.py
@@ -1,53 +1,13 @@
-import speech_recognition as sr
-import pyttsx3
-import pyaudio
-import datetime
-import wikipedia
-import pywhatkit
+from comandos import comandoDeVoz, horas, pesquisaWikipedia, tocarYoutube
+from playsound import playsound
 
-audio = sr.Recognizer()
 
-maquina = pyttsx3.init()
-maquina.setProperty('rate', 120)
+playsound("JARVIS/audio/suaDisposicao.mp3")
 
-wikipedia.set_lang('pt')
-
-def comando_de_voz():
-    try:
-        with sr.Microphone() as source:
-            print('Ouvindo...')
-            voz = audio.listen(source)
-            comando = audio.recognize_google(voz, language='pt-br')
-            comando = comando.lower()
-    except:
-        print('Wait a sec...')
-    return comando
-
-def comandos(fala):
-    if('horas' == fala):
-        hora = datetime.datetime.now().strftime('%H:%M')
-        maquina.say('Agora são' + hora)
-        maquina.runAndWait()
-    elif ('pesquisar' == fala):
-        maquina.say(f"O que pesquisar?")
-        maquina.runAndWait()
-
-        pesquisa = comando_de_voz()
-        maquina.say(f"Pesquisando {pesquisa} no wikipedia")
-        maquina.runAndWait()
-
-        resultado = wikipedia.summary(pesquisa, sentences=1)
-        maquina.say(resultado)
-        maquina.runAndWait()
-
-    elif ('tocar' == fala):
-        maquina.say("Por favor diga o nome da musica:")
-        maquina.runAndWait()
-
-        musica = comando_de_voz()
-        maquina.say(f'Tocando Musica {musica}')
-        maquina.runAndWait()
-        resultado = pywhatkit.playonyt(musica)
-
-fala = comando_de_voz()
-comandos(fala)
+comando = comandoDeVoz()
+if('horas' == comando):
+    horas()
+elif ('pesquisar' == comando):
+    pesquisaWikipedia()
+elif ('tocar' == comando):
+    tocarYoutube()
